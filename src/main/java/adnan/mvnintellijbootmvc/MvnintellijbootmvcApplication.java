05944e14204a0671b0ef4422f16b3a66c66a34e6
Adnan SSSSSSSSS <Adnan SSSSSSSSS>
2020-11-10 12:55:13 -0500

    Add rest of the project files

---
+++ src/main/java/adnan/mvnintellijbootmvc/MvnintellijbootmvcApplication.java
@@ -0,0 +1,13 @@
+package adnan.mvnintellijbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnintellijbootmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnintellijbootmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adnan/mvnintellijbootmvc/MvnintellijbootmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package adnan.mvnintellijbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnintellijbootmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
